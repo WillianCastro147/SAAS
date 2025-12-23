369a6d7a3a78708d20e50e34da0e5f394f86fed7
WillianCastro147 <eng.williancastro@gmail.com>
2025-12-23 18:32:25 -0300

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Isso aqui ignora erros de "escrita" que travam o build
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 }
-
 module.exports = nextConfig
